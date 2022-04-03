126c220414cfd82bf8760b4910a1c5bf33288900
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-03 14:58:12 -0400

    Add mobile styles for Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,20 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer">
       <Logo />
-      <address>Marthwaite, Sedbergh Cumbria +00 44 123 4567</address>
-      <h2>Open Times</h2>
-      <span>Mon - Fri: 09:00 AM - 10:00 PM</span>
-      <span>Sat - Sun: 09:00 AM - 11:30 PM</span>
+      <address className="container">
+        <span className="footer-detail">Marthwaite, Sedbergh</span>
+        <span className="footer-detail">Cumbria</span>
+        <span className="footer-detail">+00 44 123 4567</span>
+      </address>
+      <h2 className="subheading footer-detail">Open Times</h2>
+      <span className="footer-detail container">
+        Mon - Fri: 09:00 AM - 10:00 PM
+      </span>
+      <span className="footer-detail container">
+        Sat - Sun: 09:00 AM - 11:30 PM
+      </span>
     </footer>
   );
 };
