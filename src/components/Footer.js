ab46fb2c1b9342d23d074050a676758e6f62722c
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-06-19 18:56:13 -0400

    Add styles for desktop view of hero, cta and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,15 @@ const Footer = () => {
           <span className="footer-detail">Cumbria</span>
           <span className="footer-detail">+00 44 123 4567</span>
         </address>
-        <h2 className="subheading footer-detail">Open Times</h2>
-        <span className="footer-detail container">
-          Mon - Fri: 09:00 AM - 10:00 PM
-        </span>
-        <span className="footer-detail container">
-          Sat - Sun: 09:00 AM - 11:30 PM
-        </span>
+        <div>
+          <h2 className="subheading footer-detail">Open Times</h2>
+          <span className="footer-detail container">
+            Mon - Fri: 09:00 AM - 10:00 PM
+          </span>
+          <span className="footer-detail container">
+            Sat - Sun: 09:00 AM - 11:30 PM
+          </span>
+        </div>
       </div>
     </footer>
   );
